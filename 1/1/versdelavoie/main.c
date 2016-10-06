6825b8fa8e8fb2da0b625a9479edcaf228a9fce2
Dempaws <dempawswork@gmail.com>
2016-10-06 11:46:07 +0200

    Exo 1.1.3

---
+++ 1/1/versdelavoie/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int	main (int ac, char **av)
+{
+  ac = ac;
+  av = av;
+  printf("Tout droit tu grimperas,\nLa clé tu trouveras,\nHabile tu seras,\nQuand tu les porteras,\nEt avec le chef tu reviendras !");
+  return 0;
+}
