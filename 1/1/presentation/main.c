aec475af5b31a89cb008b00f44d84450cbf386d5
Dempaws <dempawswork@gmail.com>
2016-10-06 11:41:06 +0200

    Exo 1.1.2

---
+++ 1/1/presentation/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int	main (int ac, char **av)
+{
+  ac = ac;
+  av = av;
+  printf("Coucou !\nJe m'appelle Camthalion\nMa devise est 'Parler peu mais parler bien'.");
+  return 0;
+}
