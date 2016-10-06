ce1456cef975c48d8f7f7827153c00cb6e989363
Dempaws <dempawswork@gmail.com>
2016-10-06 14:41:31 +0200

    Exo1.2.1

---
+++ 1/2/punition/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int	main(int ac, char **av)
+{
+  int i;
+
+  i = 0;
+  ac = ac;
+  av = av;
+  while (i < 135)
+    {
+      printf("Coucou\n");
+      i++;
+    }
+  return (0);
+}
