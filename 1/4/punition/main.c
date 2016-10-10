1efe255cef3cb11e1d4ca26dd2a449835ff705ff
Dempaws <dempawswork@gmail.com>
2016-10-10 15:15:25 +0200

    Exo1.4.3

---
+++ 1/4/punition/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int	main(int ac, char **av)
+{
+  int	input;
+  
+  scanf("%d", &input);
+  ac = ac;
+  av = av;
+  while (input-- > 0)
+      printf("Je dois suivre en cours\n");
+    return (0);
+}
