fd97595daa1b403c6d700fad30e8031b6830066d
Dempaws <dempawswork@gmail.com>
2016-10-10 15:00:28 +0200

    Exo1.4.1

---
+++ 1/4/recoltes/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int	main(int ac, char **av)
+{
+  int	input;
+  
+  scanf("%d", &input);
+  ac = ac;
+  av = av;
+  printf("%d\n", input * input * 23);
+  return (0);
+}
