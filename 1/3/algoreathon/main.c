05d3f8fd00c92005b04b5cca1105df5caecb858f
Dempaws <dempawswork@gmail.com>
2016-10-10 13:40:22 +0200

    Exo1.3.4

---
+++ 1/3/algoreathon/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int	main(int ac, char **av)
+{
+  int	i;
+
+  i = 2 + 34 + 6;
+  ac = ac;
+  av = av;
+
+  printf ("%d %d %d\n", i, i * 2, i * 3);  
+  return (0);
+}
