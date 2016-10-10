f49ea62bf9117e784ec413981481e57e8fe7f73f
Dempaws <dempawswork@gmail.com>
2016-10-11 00:32:38 +0200

    Un commit quelquonque

--- 1/5/tarif/main.c
+++ 1/5/tarif/main.c
@@ -6,10 +6,7 @@ int	main(int ac, char **av)
   int	input1;
 
   scanf("%d", &input1);
-  input1 *= 5;
-  if (input1 > 53)
-    input1 = 53;
-  printf("%d\n", input1);
+  printf("%d\n", input1 = input1 * 5 + 10 < 53 ? input1 * 5 + 10 : 53);
   ac = ac;
   av = av;
   return (0);
