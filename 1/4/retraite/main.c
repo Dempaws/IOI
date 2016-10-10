dee2f823b4967eadd0d9630918032441cfd9b87b
Dempaws <dempawswork@gmail.com>
2016-10-10 15:05:59 +0200

    Exo1.4.2

---
+++ 1/4/retraite/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int	main(int ac, char **av)
+{
+  int	input;
+  
+  scanf("%d", &input);
+  ac = ac;
+  av = av;
+  printf("%d\n", input * 3600 * 16);
+  return (0);
+}
