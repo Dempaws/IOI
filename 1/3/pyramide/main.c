2dd68f027ea3794b21a510f2ec63cd92c3a2184f
Dempaws <dempawswork@gmail.com>
2016-10-10 14:49:11 +0200

    Exo 1.3.11

--- 1/3/pyramide/main.c
+++ 1/3/pyramide/main.c
@@ -4,7 +4,16 @@
 int	main(int ac, char **av)
 {
   int	i;
-
+  int	nbCube;
+  
+  i = 17;
+  nbCube = 0;
+  while (i > 0)
+    {
+      nbCube += i * i * i;
+      i = i - 2;
+    }
+  printf("%d\n", nbCube);
   ac = ac;
   av = av;
   return (0);
