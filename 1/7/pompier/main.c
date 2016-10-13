65608d78959092cb39c6ebca4c54def17e8bbfac
Dempaws <dempawswork@gmail.com>
2016-10-13 12:20:05 +0200

    Un commit quelquonque

--- 1/7/pompier/main.c
+++ 1/7/pompier/main.c
@@ -20,7 +20,10 @@ int	main(int ac, char **av)
       scanf("%d%d%d%d", &xmin2, &xmax2, &ymin2, &ymax2);
       if((xmin1 > xmin2 && xmin1 < xmax2) || (xmax1 > xmin2 && xmax1 < xmax2))
 	if ((ymin1 > ymin2 && ymin1 < ymax2) || (ymax1 > ymin2 && ymax1 < ymax2)) 
-	  printf("OUI\n");
+	  if (xmin1 < xmin2 || xmax1 > xmax2 || ymin1 < ymin2 || ymax1 > ymax2)
+	    printf("OUI\n");
+	  else
+	    printf("NON\n");
 	else
 	  printf("NON\n");
       else
